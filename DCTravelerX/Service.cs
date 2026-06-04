b657e1fb365f61f656f6cd282427ffb6b9b2bd88
Moe <lishvi@outlook.com>
2026-06-04 21:22:32 +0800

    Add files via upload

--- DCTravelerX/Service.cs
+++ DCTravelerX/Service.cs
@@ -4,7 +4,6 @@ using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using DCTravelerX.Helpers;
-using DCTravelerX.GameUi;
 using DCTravelerX.Infos;
 using DCTravelerX.Managers;
 using KamiToolKit;
@@ -49,7 +48,6 @@ public class Service
     internal static IDalamudPluginInterface PI        { get; private set; } = null!;
     internal static IUiBuilder              UIBuilder { get; private set; } = null!;
     internal static Configuration           Config    { get; private set; } = null!;
-    internal static TitleMenuButtonInjector? TitleMenuButtonInjector { get; private set; }
 
     public static void Init(IDalamudPluginInterface pi)
     {
@@ -67,7 +65,6 @@ public class Service
             FontManager.Init();
             WindowManager.Init();
             TitleScreenButtonManager.Init();
-            TitleMenuButtonInjector = new TitleMenuButtonInjector();
             ContextMenuManager.Init();
             IPCManager.Init();
 
@@ -85,8 +82,6 @@ public class Service
         IPCManager.Uninit();
         DCTravelClient.Instance().IsDisposed = true;
         ContextMenuManager.Uninit();
-        TitleMenuButtonInjector?.Dispose();
-        TitleMenuButtonInjector = null;
         TitleScreenButtonManager.Uninit();
         WindowManager.Uninit();
         FontManager.Uninit();
